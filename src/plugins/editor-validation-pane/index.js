9e93dcf52b0fc5568ff7716808f9bc678704af41
Kyle Shockey <shockey@users.noreply.github.com>
2018-05-08 19:38:32 -0700

    create `isValidationPaneError` predicate; filter UI errors through it

--- src/plugins/editor-validation-pane/index.js
+++ src/plugins/editor-validation-pane/index.js
@@ -1,4 +1,23 @@
+import { List } from "immutable"
+
+const isValidationPaneError = err => {
+  return err.get("type") === "spec"
+    && (err.get("source") === "schema" || err.get("source") === "semantic")
+}
+
 export default function() {
   return {
+    statePlugins: {
+      err: {
+        wrapSelectors: {
+          errorsForUiDisplay: (ori) => (...args) => {
+            return (ori(...args) || List())
+              .filter(err => {
+                return !isValidationPaneError(err)
+              })
+          }
+        }
+      }
+    }
   }
 }
