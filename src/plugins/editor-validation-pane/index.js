2f059b1813085754b4d74a8164a081882a2f8413
Kyle Shockey <shockey@users.noreply.github.com>
2018-05-08 21:42:25 -0700

    wire up validation pane into the view; modify layout styles to suit

--- src/plugins/editor-validation-pane/index.js
+++ src/plugins/editor-validation-pane/index.js
@@ -1,8 +1,18 @@
 // 1. Wrap the Errors container to filter out structural and semantic errors
 // 2. Wrap the EditorLayout to inject a ValidationPane below it
 // 3.
-
+import React from "react"
 import { List } from "immutable"
+import "./editor.less"
+
+// Wrap-Components
+
+const WrapEditorContainerWithValidationPane = (EditorContainer) => props => {
+  return <div className={`split-editor-validation-pane-container`}>
+    <EditorContainer {...props} />
+    <div className={`editor-validation-pane`}>Validation</div>
+  </div>
+}
 
 // Wrap-Selectors
 
@@ -20,8 +30,13 @@ function isValidationPaneError(err) {
     && (err.get("source") === "schema" || err.get("source") === "semantic")
 }
 
+// Plugin
+
 export default function() {
   return {
+    wrapComponents: {
+      EditorContainer: WrapEditorContainerWithValidationPane
+    },
     statePlugins: {
       err: {
         wrapSelectors: {
